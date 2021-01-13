798aa05d3785b14108e0ba1a6d0bcbf953b7b5f1
Cainar <maharamamburum@gmail.com>
2021-01-13 15:07:25 +0300

    add methods list... to User & Test models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :user_history
+
+  def self.list_tests_by_category(category)
+  	@list = []
+  	self.left_joins(:category).where('categories.title = ?', category).each do |tuple|
+  		@list << tuple.title
+  	end
+  	@list
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+	has_many :user_history
+	def list_tests_by_level level 
+		@list = []
+		UserHistory.left_joins(:user, :test)
+				   .where('user_id = ? AND tests.level = ?', self.id, level)
+				   .each do |tuple|
+			@list << tuple.test.title
+		end
+		@list
+	end
 end
