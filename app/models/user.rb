47be1935b8b18e61b622a3dd174dc3278886dbb2
Cainar <maharamamburum@gmail.com>
2021-01-27 15:13:55 +0300

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-	has_many :user_history
+	has_many :user_histories
+	has_many :tests, through: :user_histories
+	
 	def list_tests_by_level level 
 		UserHistory.left_joins(:user, :test)
 				   .where('user_id = ? AND tests.level = ?', self.id, level)
