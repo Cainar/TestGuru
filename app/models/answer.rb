017395d314b091fbe1f20ee11cf591302ae421c1
Cainar <maharamamburum@gmail.com>
2021-02-03 10:09:06 +0300

    validations added

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :body, presence: true
+  # выводит все правильные ответы
+  scope :correct_answers, -> { where(correct: true) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_132553) do
+ActiveRecord::Schema.define(version: 2021_02_02_081533) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", limit: 1000
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_01_27_132553) do
     t.boolean "is_admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
   end
 
   add_foreign_key "answers", "questions"
