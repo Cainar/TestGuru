92b9843356f1559c6a57ee388e969c1652c837cc
Cainar <maharamamburum@gmail.com>
2021-02-07 23:51:00 +0300

    sugessions processed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,46 +1,51 @@
 class QuestionsController < ApplicationController
-	before_action :find_test
+  before_action :find_test, only: %i[create index]
 
-	rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  # => rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-	def index
-		render plain: @test.questions.pluck(:id, :body)
-	end
+  def index
+    render plain: @test.questions.pluck(:id, :body)
+  end
 
-	def show
-		@question = @test.questions.find(params[:id])
-		render plain: @question.inspect
-	end
+  def show
+    if @test.present?
+      @question = @test.questions.find(params[:id])
+      render plain: @question.inspect
+    else
+      @question = Question.find(params[:id])
+      render plain: @question.inspect
+    end
+  end
 
-	def new
-		
-	end
+  def new
+    
+  end
 
-	def create
-		# result = ["Class: #{params.class}, Parameters: #{params.inspect}"]
-		#
-		# render plain: result.join("\n")
+  def create
+    # result = ["Class: #{params.class}, Parameters: #{params.inspect}"]
+    #
+    # render plain: result.join("\n")
 
-		@question = Question.create(question_params)
+    @question = @test.questions.create(question_params)
 
-		render plain: @question.inspect
-	end
+    render plain: @question.inspect
+  end
 
-	def destroy
+  def destroy
 
-	end
+  end
 
-	private
+  private
 
-	def question_params
-		params.require(:question).permit(:test_id, :body)
-	end
+  def question_params
+    params.require(:question).permit(:body)
+  end
 
-	def find_test
-		@test = Test.find(params[:test_id])
-	end
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
 
-	def rescue_with_question_not_found
-		render plain: 'Question not found'
-	end
+  def rescue_with_question_not_found
+    render plain: 'Question not found'
+  end
 end
--- db/migrate/20201226052041_add_details_to_questions.rb
+++ db/migrate/20201226052041_add_details_to_questions.rb
@@ -1,5 +1,5 @@
 class AddDetailsToQuestions < ActiveRecord::Migration[6.1]
   def change
-  	 change_column_null :questions, :body, false
+    change_column_null :questions, :body, false
   end
 end
