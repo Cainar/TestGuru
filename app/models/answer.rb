92e0c408131c6d62ea31fee8202184baee539430
Cainar <maharamamburum@gmail.com>
2021-02-07 17:59:47 +0300

    past sugessions processed

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,14 +2,15 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validates :question, presence: true, if: :answers_limit?
+  #validates :question, presence: true, if: :answers_limit?
+  validate :limit_association, on: :create
   # выводит все правильные ответы
   scope :correct_answers, -> { where(correct: true) }
 
   private
 
-  def answers_limit?
-  	unless question.answers.size < 4
+  def limit_association
+  	if question.answers.size >= 4
   		errors.add(:base, 'Question has too many answers')
   	end
   end
