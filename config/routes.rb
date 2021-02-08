11c63bd4e84ae87a0a888b88fe50120c8e99b016
Cainar <maharamamburum@gmail.com>
2021-02-08 11:59:24 +0300

    corrections added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tests, shallow: true do
-  	resources :questions
+  resources :tests do
+  	resources :questions, shallow: true
   end
-
-  get 'tests/:test_id/questions/:id', to: 'questions#show'
 end
