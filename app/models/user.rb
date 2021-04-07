c15b86c277b3ae2695d6f2f9233c2c37b472e23a
Cainar <maharamamburum@gmail.com>
2021-04-07 08:10:40 +0300

    configures Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = URI::MailTo::EMAIL_REGEXP
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+        :registerable, 
+        :recoverable, 
+        :rememberable,
+        :trackable, 
+        :validatable,
+        :confirmable
 
 	has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
   has_many :test_passages
   has_many :tests, through: :test_passages
-
-  validates :email, 
-            format: { with: VALID_EMAIL_REGEX, message: "invalid email format" },
-            uniqueness: true
-
-  has_secure_password
 
 	def list_tests_by_level(level)
 		tests.by_level(level)
--- db/migrate/20210331215537_add_email_unique_to_users.rb
+++ db/migrate/20210331215537_add_email_unique_to_users.rb
@@ -1,5 +1,9 @@
 class AddEmailUniqueToUsers < ActiveRecord::Migration[6.1]
-  def change
+  def up
     add_index :users, :email, unique: true
+  end
+
+  def down
+    remove_index :users, name: 'index_users_on_email'
   end
 end
