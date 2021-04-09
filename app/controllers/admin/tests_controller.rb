50942d5bf2e3a601596bb2da1eec18a5c048cb79
Cainar <maharamamburum@gmail.com>
2021-04-09 19:15:24 +0300

    adds Admin controller

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,45 @@
+class Admin::TestsController < Admin::BaseController
+
+  
+  before_action :set_test, only: %i[show, edit, update, destroy, start]
+  before_action :set_question, only: :show
+
+  def index
+    @tests = Test.all 
+  end
+
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def edit
+    
+  end
+
+  def create
+    
+  end
+
+  def update
+    
+  end
+
+  def start
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
+  end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
+
+  def set_question
+    
+  end
+end
