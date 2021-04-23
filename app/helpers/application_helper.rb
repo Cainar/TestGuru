6fcc3eb715d8431a5d4d5cef6252742f3ba68f41
Cainar <maharamamburum@gmail.com>
2021-04-23 08:02:55 +0300

    changes nav & flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
             "https://github.com/#{author}/#{repo}.git", 
             target: :_blank
   end
+
+  def flash_style(key)
+    case key
+    when 'notice'
+      'alert-success'
+    when 'alert'
+      'alert-danger'
+    else
+      'alert-info'
+    end
+  end
 end
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,14 @@
 module TestsHelper
+  def level_bg(test)
+    case test.level
+      when 1
+        "info"
+      when 2
+        "warning"
+      when 3
+        "danger"
+      else
+        "info"
+    end
+  end
 end
