f268e993bdebd2ba0b429a2623adc0415cdfa0cb
Cainar <maharamamburum@gmail.com>
2021-05-03 20:24:29 +0300

    adds ASC and DESC sorting for admin & user index tables

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,6 +1,6 @@
 document.addEventListener("turbolinks:load" , function() {
   var control = document.querySelector('.sort-by-title')
-  control.addEventListener('click', sortRowByTitle)
+  if (control) { control.addEventListener('click', sortRowByTitle) }
 })
 
 function sortRowByTitle() {
@@ -15,5 +15,45 @@ function sortRowByTitle() {
   for (var i = 1; i < rows.length; i++) {
     sortedRows.push(rows[i])
   }
-  console.log(sortedRows)
+
+  if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+    sortedRows.sort(compareRowsAsc)
+    this.querySelector('.octicon-arrow-up').classList.remove('hide')
+    this.querySelector('.octicon-arrow-down').classList.add('hide')
+  } else {
+    sortedRows.sort(compareRowsDesc)
+    this.querySelector('.octicon-arrow-down').classList.remove('hide')
+    this.querySelector('.octicon-arrow-up').classList.add('hide')
+  }
+
+  var sortedTable = document.createElement('table')
+  var tableBody = document.createElement('tbody')
+
+  sortedTable.classList.add('table')
+  sortedTable.appendChild(tableBody)
+  tableBody.appendChild(rows[0])
+
+  for (var i = 0; i < sortedRows.length; i++) {
+    tableBody.appendChild(sortedRows[i])
+  }
+
+  table.parentNode.replaceChild(sortedTable, table)
+}
+
+function compareRowsAsc(row1, row2) {
+  var testTitle1 = row1.querySelector('td').textContent
+  var testTitle2 = row2.querySelector('td').textContent
+
+  if (testTitle1 < testTitle2) { return -1 }
+  if (testTitle1 > testTitle2) { return 1 }
+  return 0
+}
+
+function compareRowsDesc(row1, row2) {
+  var testTitle1 = row1.querySelector('td').textContent
+  var testTitle2 = row2.querySelector('td').textContent
+
+  if (testTitle1 < testTitle2) { return 1 }
+  if (testTitle1 > testTitle2) { return -1 }
+  return 0
 }
