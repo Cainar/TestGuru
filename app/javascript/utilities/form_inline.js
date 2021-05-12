5de6dd5659481904cd4a9c48671a7563151521e6
Cainar <maharamamburum@gmail.com>
2021-05-12 23:40:57 +0300

    adds progress bar to test_passage#show

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -1,17 +1,16 @@
 document.addEventListener('turbolinks:load', function() {
-  var controls = document.querySelectorAll('.form-inline-link')
+  // var controls = document.querySelectorAll('.form-inline-link')
 
-  if (controls.length) {
-    for (var i = 0; i < controls.length; i++) {
-      controls[i].addEventListener('click', formInLineLinkHandler)
-    }
-  }
+  // if (controls.length) {
+  //   for (var i = 0; i < controls.length; i++) {
+  //     controls[i].addEventListener('click', formInLineLinkHandler)
+  //   }
+  // }
+  $('.form-inline-link').on('click', formInLineLinkHandler)
 
   var errors = document.querySelector('.resource-errors')
-  console.log(errors)
   if (errors) {
     var resourceId = errors.dataset.resourceId
-    console.log(resourceId)
     formInLineHandler(resourceId)
   }
 })
@@ -20,24 +19,20 @@ function formInLineLinkHandler(event) {
   event.preventDefault()
 
   var testId = this.dataset.testId
-  console.log(testId)
   formInLineHandler(testId)
 }
 
 function formInLineHandler(testId) {
   var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-  var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
-  var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
-  console.log(formInline)
+  var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
+  var $formInline = $('.form-inline[data-test-id="' + testId + '"]')
 
-  if (formInline.classList.contains('hide')) {
-    testTitle.classList.add('hide')
-    formInline.classList.remove('hide')
-    link.textContent = "Cancel"
-    console.log(formInline)
+  $formInline.toggle()
+  $testTitle.toggle() 
+
+  if ($formInline.is(':visible')) {
+    link.textContent = I18n.t('common.cancel')
   } else {
-    testTitle.classList.remove('hide')
-    formInline.classList.add('hide')
-    link.textContent = "Edit"
+    link.textContent = I18n.t('common.edit')
   }
 }
