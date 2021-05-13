33346b00ffd840aa69b53a50ae38c805749a0fc9
Cainar <maharamamburum@gmail.com>
2021-05-13 07:33:44 +0300

    unfinished changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import I18n from "i18n-js"
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'utilities/input_compare'
 import 'utilities/sorting'
+import I18n from 'i18n-js/index.js.erb'
 import 'utilities/form_inline'
 import 'utilities/progress_bar'
 
