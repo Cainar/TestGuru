ed44b43e60b2140d9bf033f4d241177136345677
Cainar <maharamamburum@gmail.com>
2021-05-25 06:39:49 +0300

    user role migration change

--- db/migrate/20210409095401_add_name_and_type_attributes_to_users.rb
+++ db/migrate/20210409095401_add_name_and_type_attributes_to_users.rb
@@ -1,6 +1,6 @@
 class AddNameAndTypeAttributesToUsers < ActiveRecord::Migration[6.1]
   def change
-    remove_column :users, :role, :boolean, using: 'role::boolean', :limit => nil
+    remove_column :users, :role #, :boolean, :limit => nil
     add_column :users, :type, :string, null: false, default: 'User'
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
