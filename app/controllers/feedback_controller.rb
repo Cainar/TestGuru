289a16d6fc99306ab4550f6f487716294acf1820
Cainar <maharamamburum@gmail.com>
2021-06-07 04:39:59 +0300

    recaptcha

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,5 +1,5 @@
 class FeedbackController < ApplicationController
-  before_action :set_post, only: [:destroy]
+  before_action :set_feedback, only: [:destroy]
   before_action :set_user, only: %i[create landing]
 
   def index
@@ -11,19 +11,19 @@ class FeedbackController < ApplicationController
   end
 
   def create
-    @feedback = Feedback.new(post_params)
+    @feedback = Feedback.new(feedback_params)
     @target_email = Admin.last.email
     # recaptcha пока не работает, проверку не проходит, поэтому unless
-    unless verify_recaptcha
+    if verify_recaptcha
       respond_to do |format|
         if @feedback.save
           FormMailer.new_form_email(@user, @feedback, @target_email).deliver_now
-          format.html { redirect_to '/', notice: 'The post was successfully created.' }
+          format.html { redirect_to '/', notice: 'The feedback was successfully created.' }
         end
       end
     else
       respond_to do |format|
-        format.html { redirect_to '/', alert: "Failure. Post wasn't created." }
+        format.html { redirect_to '/', alert: "Failure. feedback wasn't created." }
       end
     end
   end
@@ -31,17 +31,17 @@ class FeedbackController < ApplicationController
   def destroy
     @feedback.destroy
     respond_to do |format|
-      format.html { redirect_to feedbacks_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to feedbacks_url, notice: 'feedback was successfully destroyed.' }
     end
   end
 
   private
 
-  def set_post
+  def set_feedback
     @feedback = Feedback.find(params[:id])
   end
 
-  def post_params
+  def feedback_params
     params.require(:feedback).permit(:name, :email, :message)
   end
 
