44b9b7e33ebf18da8ebb530fbb2850e884b8c80d
Cainar <maharamamburum@gmail.com>
2021-06-07 05:07:21 +0300

    recaptcha

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -1,7 +1,8 @@
 class FormMailer < ApplicationMailer
-  def new_form_email(user, feedback, email)
+  def new_form_email(user, feedback)
     @user = user
     @feedback = feedback
-    mail to: email, subject: 'Yuo just completed the TestGuru test!'
+    @target_email = Admin.last.email
+    mail to: @target_email, subject: 'Yuo just completed the TestGuru test!'
   end
 end
